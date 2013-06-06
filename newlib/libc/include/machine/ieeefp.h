df4b7eeab5e61a11442b1cea272da66d3f23c1fd
Alessandro Pignotti <a.pignotti@sssup.it>
2013-06-06 14:58:18 +0200

    Pretend duetto is little endian

--- newlib/libc/include/machine/ieeefp.h
+++ newlib/libc/include/machine/ieeefp.h
@@ -388,6 +388,11 @@
 #define __SMALL_BITFIELDS	/* 16 Bit INT */
 #endif
 
+#if (defined(__DUETTO__))
+/* Fake little endianness */
+#define __IEEE_LITTLE_ENDIAN
+#endif
+
 #ifndef __IEEE_BIG_ENDIAN
 #ifndef __IEEE_LITTLE_ENDIAN
 #error Endianess not declared!!
