b567fa996821e1a507627ec97901a4a68050ffe4
Alessandro Pignotti <a.pignotti@sssup.it>
2013-07-04 16:18:59 +0200

    Add a script to create bc libs
    Fixing libtool is crazy

---
+++ newlib/build-bc-libs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+build_lib(){
+	mkdir -p build-bc-$1
+	cd build-bc-$1
+	ar x ../$1/$1.a
+	/opt/duetto/bin/llvm-link *.o -o $1.bc
+	cp -v $1.bc /opt/duetto/lib
+	cd ..
+	rm -rf build-bc-$1
+}
+
+build_lib libc
