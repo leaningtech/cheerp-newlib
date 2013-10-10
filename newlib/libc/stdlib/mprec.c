de68096d9ee8c6821bcf01ae106e8a8192f9ed7c
Alessandro Pignotti <a.pignotti@sssup.it>
2013-10-10 19:06:11 +0200

    Disable a custom allocator on duetto

--- newlib/libc/stdlib/mprec.c
+++ newlib/libc/stdlib/mprec.c
@@ -1,5 +1,24 @@
 /****************************************************************
  *
+ * Copyright (C) 2013 Alessandro Pignotti <alessandro@leaningtech.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * This file incorporates work covered by the following copyright and
+ * permission notice:
+ *
  * The author of this software is David M. Gay.
  *
  * Copyright (c) 1991 by AT&T.
@@ -99,6 +118,9 @@ _DEFUN (Balloc, (ptr, k), struct _reent *ptr _AND int k)
   int x;
   _Bigint *rv ;
 
+#ifdef __DUETTO__
+  rv = malloc(sizeof(struct _Bigint));
+#else
   _REENT_CHECK_MP(ptr);
   if (_REENT_MP_FREELIST(ptr) == NULL)
     {
@@ -128,6 +150,7 @@ _DEFUN (Balloc, (ptr, k), struct _reent *ptr _AND int k)
       rv->_k = k;
       rv->_maxwds = x;
     }
+#endif
   rv->_sign = rv->_wds = 0;
   return rv;
 }
@@ -135,12 +158,16 @@ _DEFUN (Balloc, (ptr, k), struct _reent *ptr _AND int k)
 void
 _DEFUN (Bfree, (ptr, v), struct _reent *ptr _AND _Bigint * v)
 {
+#ifdef __DUETTO__
+  free(ptr);
+#else
   _REENT_CHECK_MP(ptr);
   if (v)
     {
       v->_next = _REENT_MP_FREELIST(ptr)[v->_k];
       _REENT_MP_FREELIST(ptr)[v->_k] = v;
     }
+#endif
 }
 
 _Bigint *
