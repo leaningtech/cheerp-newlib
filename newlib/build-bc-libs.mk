fd1223a5f2d4eedbc376e0581f7e7ee02934b9ec
Alessandro Pignotti <a.pignotti@sssup.it>
2013-11-27 09:35:58 +0100

    Clean up debian build

--- newlib/build-bc-libs.mk
+++ newlib/build-bc-libs.mk
@@ -1,6 +1,6 @@
 %.bc: %.a
 	mkdir -p build_bc_tmp
-	cd build_bc_tmp; ar x $^; /opt/duetto/bin/llvm-link *.o -o $@
+	cd build_bc_tmp; ar x ../$^; /opt/duetto/bin/llvm-link *.o -o ../$@
 	rm -rf build_bc_tmp
 
-all: $(CURDIR)/$(DESTDIR)/opt/duetto/lib/libc.bc $(CURDIR)/$(DESTDIR)/opt/duetto/lib/libm.bc
+all: build/libc/libc.bc build/libm/libm.bc
