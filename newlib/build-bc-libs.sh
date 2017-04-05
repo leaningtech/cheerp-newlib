0cc65d628d9afa9c36084397137e409b17caaa82
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-04-05 09:24:53 +0000

    New location for standard libs

--- newlib/build-bc-libs.sh
+++ newlib/build-bc-libs.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
+if [ -z $CHEERP_PREFIX ]; then
+	CHEERP_PREFIX=/opt/cheerp
+fi
+
+if [ "$1" != "genericjs" ] && [ "$1" != "asmjs" ]; then
+	echo "first argument must be 'genericjs' or 'asmjs'"
+	exit 1
+fi
+
 build_lib(){
 	mkdir -p build-bc-$1
 	cd build-bc-$1
 	ar x ../$1/$1.a
-	/opt/cheerp/bin/llvm-link *.o -o $1.bc
-	cp -v $1.bc /opt/cheerp/lib
+	$CHEERP_PREFIX/bin/llvm-link *.o -o $1.bc
+	mkdir -p $CHEERP_PREFIX/lib/$2
+	cp -v $1.bc $CHEERP_PREFIX/lib/$2/$1.bc
 	cd ..
 	rm -rf build-bc-$1
 }
 
-build_lib libc
-build_lib libm
+build_lib libc $1
+build_lib libm $1
